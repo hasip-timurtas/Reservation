2422fabbce3374b41f821d514ec3a49d3cd021de
hasip.timurtas <hasip.timurtas@gmail.com>
2016-10-24 14:30:49 +0300

    hasip

--- client/App/App.js
+++ client/App/App.js
@@ -16,6 +16,7 @@ export class App extends React.Component {
             <div className="row">
               <ShowRooms />
               <br />
+			  <p> Hasip </p>
               <NewReservation />
             </div>
         )
